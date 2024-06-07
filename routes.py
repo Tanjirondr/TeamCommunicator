a4da707552c70f351170f1f6d51e1501465d6826
Tanjirondr <121744686+Tanjirondr@users.noreply.github.com>
2024-06-06 22:32:03 -0400

    added function

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+from flask_sql_javaalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ class TeamRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(300), nullable=False)
-    comments = db.relationship('Comment', backref='team_request', lazy=True)
+    comments = db.relationship('Comment', backref='team_request', lazy=True, cascade="all, delete-orphan")
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,7 +23,7 @@ class Comment(db.Model):
 def get_all_requests():
     all_requests = TeamRequest.query.all()
     requests_list = []
-    for team_request in all_requests:
+    for team_request in all_instances:
         request_details = {'id': team_request.id, 'title': team_request.title, 'description': team_request.description}
         requests_list.append(request_details)
     return jsonify({'requests': requests_list})
@@ -39,7 +39,13 @@ def create_team_request():
 @app.route('/requests/<int:team_request_id>', methods=['GET'])
 def get_single_request(team_request_id):
     team_request = TeamRequest.query.get_or_404(team_request_id)
-    return jsonify({'id': team_request.id, 'title': team_request.title, 'description': team_request.description})
+    request_details = {
+        'id': team_request.id,
+        'title': team_request.title,
+        'description': team_request.description,
+        'comments': [{'id': comment.id, 'content': comment.content} for comment in team_request.comments]
+    }
+    return jsonify(request_details)
 
 @app.route('/requests/<int:team_request_id>/comments', methods=['POST'])
 def add_comment_to_team_request(team_request_id):
@@ -49,6 +55,13 @@ def add_comment_to_team_request(team_request_id):
     db.session.add(new_comment)
     db.session.commit()
     return jsonify({'message': 'Comment added to Team Request successfully'}), 201
+
+@app.route('/comments/<int:comment_id>', methods=['DELETE'])
+def delete_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    db.session.delete(comment)
+    db.session.commit()
+    return jsonify({'message': 'Comment deleted successfully'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
